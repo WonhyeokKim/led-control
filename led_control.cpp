cc7849d3627e7d6f9a659f0ff344caa5d9096295
Wonhyeok Kim <80291720+WonhyeokKim@users.noreply.github.com>
2021-08-27 13:00:59 +0900

    feat: LED Spectrom

--- led_control.cpp
+++ led_control.cpp
@@ -35,3 +35,37 @@ void LedControl::led_on() {
 void LedControl::led_off() {
 	digitalWrite(pin, 0);
 }
+
+LedSpectrom::LedSpectrom(int minimum_led_pin, int maximum_led_pin) {
+    minimum_pin = minimum_led_pin;
+    maximum_pin = maximum_led_pin;
+    
+    for (int i = minimum_pin; maximum_pin >= i; i++) {
+        pinMode(i, OUTPUT);
+    }
+}
+
+void LedSpectrom::led_spectrom(int button_pin, int delay_ms, bool upper) {
+    pinMode(button_pin, INPUT_PULLUP);
+    if (digitalRead(button_pin) != 1) {
+        if (upper) {
+            for (int i = minimum_pin; maximum_pin >= i; i++) {
+                digitalWrite(i, 1);
+                delay(delay_ms);
+            }
+        } else {
+            for (int i = maximum_pin; i >= minimum_pin; i--) {
+                pinMode(i, OUTPUT);
+                digitalWrite(i, 1);
+                delay(delay_ms);
+            }
+        }
+    
+        for (int i = minimum_pin; maximum_pin >= i; i++) {
+            pinMode(i, OUTPUT);
+            digitalWrite(i, 0);
+        }
+        
+        delay(delay_ms);
+    }
+}
