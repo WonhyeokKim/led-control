0e3a3531ec4a52743e5ec1504355599efff9b418
Wonhyeok Kim <80291720+WonhyeokKim@users.noreply.github.com>
2021-08-27 13:01:32 +0900

    feat: Led Spectrom Header

--- led_control.h
+++ led_control.h
@@ -15,4 +15,13 @@ class LedControl {
 		void led_on();
 		void led_off();
 };
+
+class LedSpectrom {
+    private:
+        int minimum_pin;
+        int maximum_pin;
+    public:
+        LedSpectrom(int minimum_led_pin, int maximum_led_pin);
+        void led_spectrom(int button_pin, int delay, bool upper);
+};
 #endif
