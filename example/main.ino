da9135db4cad4915a7547d71e2efe14154a277a3
Wonhyeok Kim <80291720+WonhyeokKim@users.noreply.github.com>
2021-08-30 08:46:55 +0900

    Update main.ino

--- example/main.ino
+++ example/main.ino
@@ -2,6 +2,12 @@
 
 #define LED 12
 
+#define S_LED 7
+#define E_LED 12
+
+#define BUTTON_0
+#define BUTTON_1
+
 void setup() {}
 
 void loop() {
@@ -10,4 +16,8 @@ void loop() {
   delay(1000);
   control.led_off();
   delay(1000);
+  
+  LedSpectrom spectrom = LedSpectrom(S_LED, E_LED);
+  spectrom.led_spectrom(BUTTON_0, 500, true);
+  spectrom.led_spectrom(BUTTON_1, 500, false);
 }
