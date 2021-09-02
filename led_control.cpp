ba783064de4037b524963b4ae4e9775c36309d92
Wonhyeok Kim <80291720+WonhyeokKim@users.noreply.github.com>
2021-09-02 11:05:25 +0900

    feat: potention meter

--- led_control.cpp
+++ led_control.cpp
@@ -4,9 +4,11 @@
 LedControl::LedControl(int led_pin) {
 	pinMode(led_pin, OUTPUT);
 	pin = led_pin;
+	
+	prefix = "[LedControl] ";
 }
 
-void LedControl::button_led(int button_pin) {
+void LedControl::button_control(int button_pin) {
 	pinMode(button_pin, INPUT_PULLUP);
 	if (digitalRead(button_pin) != 1) {
 		digitalWrite(pin, 1);
@@ -15,7 +17,7 @@ void LedControl::button_led(int button_pin) {
 	}
 }
 
-void LedControl::button_led(int button_pin, int delay_sec) {
+void LedControl::button_control(int button_pin, int delay_sec) {
 	pinMode(button_pin, INPUT_PULLUP);
 	if (digitalRead(button_pin) != 1) {
 		digitalWrite(pin, 1);
@@ -36,9 +38,36 @@ void LedControl::led_off() {
 	digitalWrite(pin, 0);
 }
 
+void LedControl::pot_control(int pot_pin, int mode, bool debug) {
+	switch (mode) {
+		case 0:
+			if (analogRead(pot_pin) < 500) {
+				LedControl::led_on();
+			} else {
+				LedControl::led_off();
+			}
+	
+			if (debug) {
+				Serial.println(prefix + String(analogRead(pot_pin)));
+			}
+			
+			break;
+			
+		case 1:
+			LedControl::led_on();
+			delay(analogRead(pot_pin));
+			LedControl::led_off();
+			delay(analogRead(pot_pin));
+			
+			break;
+	}
+}
+
 LedSpectrom::LedSpectrom(int minimum_led_pin, int maximum_led_pin) {
     minimum_pin = minimum_led_pin;
     maximum_pin = maximum_led_pin;
+    
+    prefix = "[LedSpectrom] ";
     
     for (int i = minimum_pin; maximum_pin >= i; i++) {
         pinMode(i, OUTPUT);
